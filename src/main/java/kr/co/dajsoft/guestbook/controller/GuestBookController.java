3cd1ad7c0a6067c86c8bfab8c71efa76ba608521
DESKTOP-TIIHLUS\TJ_704 <future1develope@kakao.com>
2022-01-17 10:56:58 +0900

    commit1

--- src/main/java/kr/co/dajsoft/guestbook/controller/GuestBookController.java
+++ src/main/java/kr/co/dajsoft/guestbook/controller/GuestBookController.java
@@ -3,9 +3,11 @@ package kr.co.dajsoft.guestbook.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-//PageController를 만들기 위한 어노테이션
+
 @Log4j2
 //로그 출력을 위한 어노테이션
+
+//PageController를 만들기 위한 어노테이션
 @Controller
 public class GuestBookController {
     @GetMapping("/")
